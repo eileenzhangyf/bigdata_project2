e56b532b19b7eaacf7390f1088c91cb09a04c846
eileenzhangyf <45110207+eileenzhangyf@users.noreply.github.com>
2021-03-24 14:48:27 -0400

    Update main.scala

--- src/main/scala/project_2/main.scala
+++ src/main/scala/project_2/main.scala
@@ -116,7 +116,7 @@ object main{
 
 
   def exact_F2(x: RDD[String]) : Long = {
-    val ans=x.map(i =>x.reduceByKey(_ + _)).foldleft(0)(_+_._1)
+    val ans=x.map(i => math.pow(i.count,2).toInt).reduceByKey(_ + _)
     return ans
   }
 
